af23ed496ec9436c9ff00caf3a625a3cf5c8a05b
huyvp <nvh1892kw@gmail.com>
2025-10-15 23:03:12 +0700

    add 1st protect with resilience4j

--- controller-module/src/main/java/com/source/controller/resource/Controller.java
+++ controller-module/src/main/java/com/source/controller/resource/Controller.java
@@ -1,7 +1,9 @@
 package com.source.controller.resource;
 
 import com.source.application.service.event.EventService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,13 @@ public class Controller {
     @Autowired
     private EventService eventService;
 
+    @GetMapping(path = "/hi")
+    @RateLimiter(name = "backendA", fallbackMethod = "fallbackMethod")
+    public String hello() {
+        return "hello";
+    }
 
+    private String fallbackMethod() {
+        return "fallback";
+    }
 }
